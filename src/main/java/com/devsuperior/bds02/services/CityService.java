9f8ef3a154bfc3ecb9a557b4166697b2fc2d85d9
Breno <breno.joaosalvadorperez200@gmail.com>
2026-09-11 09:20:23 -0300

    feat: implement CityController REST endpoints

--- src/main/java/com/devsuperior/bds02/services/CityService.java
+++ src/main/java/com/devsuperior/bds02/services/CityService.java
@@ -2,12 +2,14 @@ package com.devsuperior.bds02.services;
 
 import com.devsuperior.bds02.dto.CityDTO;
 import com.devsuperior.bds02.entities.City;
+import com.devsuperior.bds02.exceptions.DatabaseException;
 import com.devsuperior.bds02.exceptions.ResourceNotFoundException;
 import com.devsuperior.bds02.repositories.CityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -34,7 +36,7 @@ public class CityService {
         return new CityDTO(entity);
     }
 
-    @Transactional
+    @Transactional(propagation = Propagation.SUPPORTS)
     public void delete(Long id) {
         if (!repository.existsById(id)) {
             throw new ResourceNotFoundException("Recurso não encontrado");
